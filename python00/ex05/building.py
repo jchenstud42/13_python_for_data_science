a27d0e58cebf49e0880a74a2f9dda519a509827f
Julie Chen <jchen@f2r7s18.clusters.42paris.fr>
2026-02-03 15:42:43 +0100

    ex05 oki doki

---
+++ python00/ex05/building.py
@@ -0,0 +1,47 @@
+import sys
+
+BOLD = "\033[1m"
+RED = "\033[91m"
+RESET = "\033[0m"
+
+
+def character_calculator() -> int:
+    """Displays the sums of its upper-case characters,
+    lower-case characters, punctuation, digits, and spaces"""
+    try:
+        assert len(sys.argv) < 3, "Too many arguments"
+
+        if len(sys.argv) == 1:
+            print(f"{BOLD}What is the text to count?{RESET}")
+            inp = input()
+            print("")
+        else:
+            inp = sys.argv[1]
+
+        upper = sum(1 for c in inp if c.isupper())
+        lower = sum(1 for c in inp if c.islower())
+        punct = sum(1 for x in inp if x in "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}")
+        space = inp.count(" ")
+        digit = sum(1 for c in inp if c.isdigit())
+
+        print(
+            f"{BOLD}The text contains {len(inp)} characters:\n{RESET}",
+            f"{BOLD}{upper}{RESET} upper letters\n",
+            f"{BOLD}{lower}{RESET} lower letters\n",
+            f"{BOLD}{punct}{RESET} punctuation marks\n",
+            f"{BOLD}{space}{RESET} spaces\n",
+            f"{BOLD}{digit}{RESET} digits",
+        )
+
+    except AssertionError as error:
+        print(f"{RED}AssertionError: {error}{RESET}")
+
+
+def main():
+    character_calculator()
+
+
+# -------------------- FUNCTION CALL --------------------
+
+if __name__ == "__main__":
+    main()
