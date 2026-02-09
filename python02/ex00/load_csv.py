fcfa2d94e161c9de5bc61f01a2d5776c5f7337f4
Julie Chen <jchen@f4r13s6.clusters.42paris.fr>
2026-02-09 16:32:19 +0100

    p02 ex00

---
+++ python02/ex00/load_csv.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+
+RED = "\033[91m"
+BOLD = "\033[1m"
+RESET = "\033[0m"
+
+
+def load(path: str):
+    """Writes the dimensions of the data set, and returns it"""
+    if not os.path.exists(path):
+        print(f"{RED}Error: file '{path}' not found{RESET}")
+        return None
+
+    ext = os.path.splitext(path)[1]
+    if ext != ".csv":
+        print(f"{RED}Error: only .csv files allowed{RESET}")
+        return None
+
+    try:
+        datafile = pd.read_csv(path)
+        print(f"{BOLD}\n---- Loading dataset of dimensions {datafile.shape}{RESET}\n")
+
+        return datafile
+
+    except Exception as error:
+        print(f"{RED}failed to load {path}: {error}{RESET}")
+        return None
+
+
+def main():
+    try:
+        print(load("../life_expectancy_years.csv"))
+
+    except AssertionError as error:
+        print(f"{RED}AssertionError: {error}{RESET}")
+
+
+# -------------------- FUNCTION CALL --------------------
+
+if __name__ == "__main__":
+    main()
