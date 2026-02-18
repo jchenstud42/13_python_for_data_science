83934142685a11ea4d6ce96d2f189efbd27a7632
Julie Chen <jchen@f2r7s9.clusters.42paris.fr>
2026-02-18 12:43:45 +0100

    p03 ok

---
+++ python03/ex04/ft_calculator.py
@@ -0,0 +1,51 @@
+RED = "\033[91m"
+BOLD = "\033[1m"
+RESET = "\033[0m"
+
+
+# decorator : fonction qui modifie le comportement d'autres ft/methodes
+# en ajoutant des fonctionnalites supplementaires av ou apres leur exec
+
+
+class Calculator:
+    """Class used for arithmetic operations"""
+
+    # staticmethod : permet d'indiquer une methode qui fonctionne
+    # independamment des instances de classe. Ne peut pas modifier
+    # l'etat d'un objet -> ft utilitaire
+    @staticmethod
+    def dotproduct(V1: list[float], V2: list[float]) -> None:
+        """Multiply each element of two vectors"""
+        result = sum(x * y for x, y in zip(V1, V2))
+        print(f"Dot product is : {result}")
+
+    @staticmethod
+    def add_vec(V1: list[float], V2: list[float]) -> None:
+        """Adds each element of two vectors"""
+        result = [x + y for x, y in zip(V1, V2)]
+        print(f"Add Vector is : {result}")
+
+    @staticmethod
+    def sous_vec(V1: list[float], V2: list[float]) -> None:
+        """Substracts each element of two vectors"""
+        result = [x - y for x, y in zip(V1, V2)]
+        print(f"Sous Vector is : {result}")
+
+
+def main():
+    try:
+        a = [5.0, 10.0, 2.0]
+        b = [2.0, 4.0, 3.0]
+        Calculator.dotproduct(a, b)
+        Calculator.add_vec(a, b)
+        Calculator.sous_vec(a, b)
+
+    except RuntimeError as error:
+        print(f"{RED}Error: {error}{RESET}")
+
+
+# -------------------- FUNCTION CALL --------------------
+
+
+if __name__ == "__main__":
+    main()
